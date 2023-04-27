89aedb64cf6e2770f849548e8267c2d0d59223c0
Yang Le <yangle@redhat.com>
2023-04-27 17:54:34 +0800

    set priority & severity of test cases
    Signed-off-by: Yang Le <yangle@redhat.com>

--- build/start_tests.sh
+++ build/start_tests.sh
@@ -14,8 +14,19 @@ echo "Tests <$TEST_GROUP> start at "$(date)
 
 if [[ $TEST_GROUP == "placement" ]]; then
     /placement-e2e --ginkgo.v --ginkgo.label-filter=sanity-check \
-     --ginkgo.junit-report="${RESULTS_DIR}/foundation-placement-e2e.xml" \
-     -hub-kubeconfig=${HUB_KUBECONFIG} -create-global-clusterset=false -tolerate-unreachable-taint
+        --ginkgo.junit-report="${RESULTS_DIR}/foundation-placement-e2e.xml" \
+        -hub-kubeconfig=${HUB_KUBECONFIG} -create-global-clusterset=false -tolerate-unreachable-taint
+    ret=$?
+
+    #set the priority and severity of the test cases if report exists
+    if [ -e ${RESULTS_DIR}/foundation-placement-e2e.xml ]
+    then
+        sed -i -e 's/\[It\]/Server Foundation: \[P2\]\[Sev2\]\[server-foundation\]/' \
+            -e 's/"\[BeforeSuite\]"/"Server Foundation: \[P2\]\[Sev2\]\[server-foundation\] BeforeSuite"/' \
+            -e 's/"\[AfterSuite\]"/"Server Foundation: \[P2\]\[Sev2\]\[server-foundation\] AfterSuite"/' \
+            ${RESULTS_DIR}/foundation-placement-e2e.xml
+    fi
+    exit $ret
 elif [[ $TEST_GROUP == "work" ]]; then
     echo "Not implemented yet for $TEST_GROUP"
 fi
